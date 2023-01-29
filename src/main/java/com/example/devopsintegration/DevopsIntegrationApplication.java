4c755a142d8a3f5ba1d55d05f782d7d8371f1cce
polachandu <chandrahaspola@gmail.com>
2023-01-29 13:04:45 -0800

    Second commit

--- src/main/java/com/example/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/example/devopsintegration/DevopsIntegrationApplication.java
@@ -2,10 +2,11 @@ package com.example.devopsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class DevopsIntegrationApplication {
 
